843fee458cd917f6bf2c77b1e90261094bdfe1a7
Ridho Perdana <ridho.perdana@wartek.belajar.id>
2023-03-18 09:37:47 +0700

    feat: table sortable

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Box } from '@mui/material';
+import axios from 'axios';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { orderBy } from 'lodash';
+import { ArrowDropDown, ArrowDropUp } from '@mui/icons-material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2',
+    },
+  },
+});
 
 function App() {
+  const [data, setData] = useState([]);
+  const [sortedData, setSortedData] = useState([]);
+  const [sortDirection, setSortDirection] = useState('asc');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://jsonplaceholder.typicode.com/posts'
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
+  const handleSort = () => {
+    const nextSortDirection = sortDirection === 'asc' ? 'desc' : 'asc';
+    const sorted = orderBy(data, 'id', nextSortDirection);
+    setSortedData(sorted);
+    setSortDirection(nextSortDirection);
+  };
+
+  const renderSortIcon = () => {
+    if (sortDirection === 'asc') {
+      return <ArrowDropDown />;
+    } else {
+      return <ArrowDropUp />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ padding: '30px' }}>
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead sx={{ backgroundColor: theme.palette.primary.main }}>
+              <TableRow>
+                <TableCell sx={{ color: 'white', cursor: 'pointer' }} onClick={handleSort}>
+                  <Box display="flex" alignItems="center">
+                    ID
+                    <IconButton onClick={handleSort} size="small">
+                      {renderSortIcon()}
+                    </IconButton>
+                  </Box>
+                </TableCell>
+                <TableCell sx={{ color: 'white' }}>Title</TableCell>
+                <TableCell sx={{ color: 'white' }}>Body</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {(sortedData.length > 0 ? sortedData : data).map((item) => (
+                <TableRow key={item.id}>
+                  <TableCell component="th" scope="row">
+                    {item.id}
+                  </TableCell>
+                  <TableCell>{item.title}</TableCell>
+                  <TableCell>{item.body}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </ThemeProvider>
   );
 }
 
