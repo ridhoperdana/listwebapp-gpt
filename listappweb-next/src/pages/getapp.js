1e5f02888382cf3f1eba622cc72aa0a1eaf105fe
Ridho Perdana <ridho.perdana@wartek.belajar.id>
2023-03-20 21:25:49 +0700

    feat: add redis

--- listappweb-next/src/pages/getapp.js
+++ listappweb-next/src/pages/getapp.js
@@ -1,6 +1,29 @@
 import { exec } from 'child_process';
+import util from 'util';
+// import redis from 'redis';
+import { Redis } from "ioredis";
+
+// const client = redis.createClient();
+// await client.connect();
+// client.get = util.promisify(client.get);
+const redis = new Redis();
 
 export async function getAppData() {
+    const cacheKey = 'appData';
+
+    // Check if the result is already in cache
+    const cachedResult = await redis.get(cacheKey);
+    if (cachedResult) {
+        return JSON.parse(cachedResult);
+    }
+
+    const result = await getAppDataFromTerminal();
+    // Store the result in cache for 1 hour
+    await redis.set(cacheKey, JSON.stringify(result), 'EX', 3600);
+    return result;
+};
+
+export async function getAppDataFromTerminal() {
     return new Promise((resolve, reject) => {
         const child = exec('lsof -i | grep "LISTEN"');
         let buffer = '';
